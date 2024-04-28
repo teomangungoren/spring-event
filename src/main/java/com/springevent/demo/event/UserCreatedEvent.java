d9ab065af07bb505768bc4b90a292e75de0b8eb7
teomangungoren <teomangungoren@gmail.com>
2024-04-28 17:49:49 +0300

    fix

--- src/main/java/com/springevent/demo/event/UserCreatedEvent.java
+++ src/main/java/com/springevent/demo/event/UserCreatedEvent.java
@@ -17,22 +17,6 @@ public class UserCreatedEvent {
         this.user = user;
     }
 
-    @Override
-    public String toString() {
-        return "CustomerCreatedEvent{" +
-                "customer=" + user +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UserCreatedEvent that = (UserCreatedEvent) o;
-
-        return user.equals(that.user);
-    }
 
 
 }
